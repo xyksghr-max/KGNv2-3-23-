9f5fad19431c3cd14f37b2b23013d6ed35810af2
xyksghr-max <xyksghr@gmail.com>
2026-03-25 14:11:32 +0800

    chore: 恢复云端全量数据生成与满血训练参数，更新测试脚本权重路径

--- experiments/test_kgnv2.sh
+++ experiments/test_kgnv2.sh
@@ -23,7 +23,7 @@ for (( i=0;i<${#DIST_TH_LIST[@]};i++)); do
         --dataset ps_grasp \
         --ps_data_mode ${TEST_DATA_MODE} \
         --keep_res \
-        --load_model ../exp/kgnv2.pth \
+        --load_model ../exp/grasp_pose/kgnV2_${TRAIN_DATA_MODE}/model_last.pth \
         --not_prefetch_test \
         --trainval \
         --kpt_type box \
